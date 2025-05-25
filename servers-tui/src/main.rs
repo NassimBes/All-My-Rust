1f403f382a8a11a30834911905162e1ed0184939
NassimBes <nassim.besri@gmail.com>
2025-05-25 22:16:10 +0100

    New Project ServerTUI

--- servers-tui/src/main.rs
+++ servers-tui/src/main.rs
@@ -1,59 +1,71 @@
-use color_eyre::{
-    eyre::{bail, WrapErr},
-    Result,
+
+use color_eyre::{eyre::Ok, Result};
+use crossterm::{event::{self, Event}, terminal};
+use ratatui::{
+    layout::{self, Constraint, Direction, Layout, Rect}, prelude::CrosstermBackend, widgets::{Block, Paragraph}, DefaultTerminal, Frame, Terminal
+    
 };
 
-pub mod app;
-use app::App;
+mod app {
+    use std::rc::Rc;
 
-
-
-fn main() -> Result<()> {
-    color_eyre::install()?;
-    let mut terminal = app::tui::init()?;
-    let app_result = App::default().run(&mut terminal);
-    if let Err(err) = app::tui::restore() {
-        eprintln!(
-            "failed to restore terminal. Run `reset` or restart your terminal to recover: {}",
-            err
-        );
-    }
-    app_result
-}
-
-
-#[cfg(test)]
-mod tests {
     use super::*;
-    use ratatui::{
-        buffer::Buffer,
-        layout::Rect,
-        style::{Style, Stylize},
-        widgets::{Widget},
-    };
-
-    #[test]
-    fn render() {
-        let app = App::default();
-        let mut buf = Buffer::empty(Rect::new(0, 0, 50, 4));
-
-        app.render(buf.area, &mut buf);
-
-        let mut expected = Buffer::with_lines(vec![
-            "┏━━━━━━━━━━━━━ Counter App Tutorial ━━━━━━━━━━━━━┓",
-            "┃                    Value: 0                    ┃",
-            "┃                                                ┃",
-            "┗━ Decrement <Left> Increment <Right> Quit <Q> ━━┛",
-        ]);
-        let title_style = Style::new().bold();
-        let counter_style = Style::new().yellow();
-        let key_style = Style::new().blue().bold();
-        expected.set_style(Rect::new(14, 0, 22, 1), title_style);
-        expected.set_style(Rect::new(28, 1, 1, 1), counter_style);
-        expected.set_style(Rect::new(13, 3, 6, 1), key_style);
-        expected.set_style(Rect::new(30, 3, 7, 1), key_style);
-        expected.set_style(Rect::new(43, 3, 4, 1), key_style);
-
-        assert_eq!(buf, expected);
+    
+    pub fn run(mut terminal: Terminal<CrosstermBackend<std::io::Stdout>>) -> std::result::Result<(), color_eyre::eyre::Error>{
+        loop {
+            terminal.draw(render)?;
+            if matches!(event::read()?, Event::Key(_)) {
+                break Ok(())
+            }
+        }
     }
+
+    fn render(frame: &mut Frame){
+        let layout = Layout::new(
+        Direction::Horizontal, 
+        [Constraint::Percentage(15),Constraint::Percentage(70),Constraint::Percentage(15)],
+        )
+        .split(frame.area());
+
+        render_left_block(frame,&layout);
+        render_middle_block(frame,&layout);
+        render_right_block(frame,&layout);
+        
+        fn render_left_block(frame: &mut Frame, layout: &Rc<[Rect]> ){
+                frame.render_widget(Paragraph::default()
+                .block(Block::bordered()
+                .title("Left Block")
+                .title_alignment(layout::Alignment::Center)
+            )
+                , layout[0]);
+        }
+        fn render_middle_block(frame: &mut Frame, layout: &Rc<[Rect]>){
+            frame.render_widget(Paragraph::default()
+            .block(Block::bordered()
+            .title("Chat Room")
+            .title_alignment(layout::Alignment::Center)
+        )
+            , layout[1]);
+        }
+
+        fn render_right_block(frame: &mut Frame, layout: &Rc<[Rect]>){
+            frame.render_widget(Paragraph::default()
+            .block(Block::bordered()
+            .title("Users in Current Chat room").title_alignment(layout::Alignment::Center).
+        )
+            , layout[2]);
+        }
+    }
+    
 }
+
+fn main() -> Result<()>{
+   color_eyre::install()?;
+   let terminal: Terminal<CrosstermBackend<std::io::Stdout>> = ratatui::init();
+   let result = app::run(terminal);
+
+   ratatui::restore();
+   result
+}
+
+
