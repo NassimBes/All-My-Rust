efa58b191826db410a25690ceaf80c6a559c5be6
NassimBes <nassim.besri@gmail.com>
2025-05-27 00:27:13 +0100

    HardCoding Rooms names

--- servers-tui/src/main.rs
+++ servers-tui/src/main.rs
@@ -1,22 +1,33 @@
 
 use color_eyre::{eyre::Ok, Result};
-use crossterm::{event::{self, Event}, terminal};
 use ratatui::{
-    layout::{self, Constraint, Direction, Layout, Rect}, prelude::CrosstermBackend, widgets::{Block, Paragraph}, DefaultTerminal, Frame, Terminal
-    
+    prelude::CrosstermBackend,
+    Terminal
 };
 
+
 mod app {
+    use super::*;
+    #[allow(unused_imports)]
+    use ratatui::{
+        layout::{self, Constraint, Direction, Layout, Rect},
+        style::{Color, Style}, 
+        widgets::{Block, Borders, List, ListItem, Paragraph},
+        Frame
+    };
+
+    use crossterm::{event::{self, Event, KeyCode, KeyEvent}, style};
     use std::rc::Rc;
 
-    use super::*;
-    
-    pub fn run(mut terminal: Terminal<CrosstermBackend<std::io::Stdout>>) -> std::result::Result<(), color_eyre::eyre::Error>{
+
+    pub fn run(mut terminal: Terminal<CrosstermBackend<std::io::Stdout>>) -> Result<()>{
         loop {
             terminal.draw(render)?;
-            if matches!(event::read()?, Event::Key(_)) {
-                break Ok(())
-            }
+            if matches!(event::read()?, Event::Key(KeyEvent{
+                    code: KeyCode::Esc,
+                    ..}
+                )
+            ){ break Ok(()) }
         }
     }
 
@@ -27,35 +38,63 @@ mod app {
         )
         .split(frame.area());
 
-        render_left_block(frame,&layout);
-        render_middle_block(frame,&layout);
-        render_right_block(frame,&layout);
-        
-        fn render_left_block(frame: &mut Frame, layout: &Rc<[Rect]> ){
-                frame.render_widget(Paragraph::default()
-                .block(Block::bordered()
-                .title("Left Block")
-                .title_alignment(layout::Alignment::Center)
-            )
-                , layout[0]);
-        }
-        fn render_middle_block(frame: &mut Frame, layout: &Rc<[Rect]>){
-            frame.render_widget(Paragraph::default()
-            .block(Block::bordered()
-            .title("Chat Room")
-            .title_alignment(layout::Alignment::Center)
-        )
-            , layout[1]);
-        }
-
-        fn render_right_block(frame: &mut Frame, layout: &Rc<[Rect]>){
-            frame.render_widget(Paragraph::default()
-            .block(Block::bordered()
-            .title("Users in Current Chat room").title_alignment(layout::Alignment::Center).
-        )
-            , layout[2]);
-        }
+        render_rooms_block(frame,&layout);
+        render_chatroom_block(frame,&layout);
+        render_chatter_block(frame,&layout);
     }
+    
+    fn render_rooms_block(frame: &mut Frame, layout: &Rc<[Rect]> ){  
+        let chatrooms = vec!["Total","Total","Total","Total","Total","Total"];
+        let binding = chatrooms.iter().map(|&f| {
+            ListItem::new(f)
+            .style(Style::default().fg(Color::Green))
+        });
+
+        frame.render_widget(List::new(binding)
+        .block(
+            Block::bordered()
+            .title("Rooms").title_alignment(layout::Alignment::Center)
+        )
+        .highlight_symbol(">>>")
+        .highlight_spacing(ratatui::widgets::HighlightSpacing::Always)
+        , layout[0]);
+        // frame.render_widget(
+        // List::new([
+        //     Text::styled("Total", Style::default().fg(Color::Red)),
+        //     Text::styled("Total", Style::default().fg(Color::Red)),
+        //     Text::styled("Total", Style::default().fg(Color::Red)),
+        //     Text::styled("Total", Style::default().fg(Color::Red)),
+        //     Text::styled("Total", Style::default().fg(Color::Red)),
+        // ]).block(
+        //     Block::bordered()
+        //     .title("Rooms")
+        //     .title_alignment(layout::Alignment::Center)
+        // ),
+        // layout[0]);
+        
+    }
+    fn render_chatroom_block(frame: &mut Frame, layout: &Rc<[Rect]>){
+        frame.render_widget(
+    Paragraph::default()
+                .block(
+                    Block::bordered()
+                        .title("Chat Room")
+                        .title_alignment(layout::Alignment::Center)
+                )
+        , layout[1]);
+    }
+
+    fn render_chatter_block(frame: &mut Frame, layout: &Rc<[Rect]>){
+        frame.render_widget(
+    Paragraph::default()
+                .block(
+                    Block::bordered()
+                        .title("Users in Current Chat room")
+                        .title_alignment(layout::Alignment::Center)
+                )
+        , layout[2]);
+    }
+    
     
 }
 
